968b1e1f6870792cb374e5f2a1853e63d3588ff5
Josianjidile <josianjidile@gmail.com>
2023-07-30 23:16:32 +0300

    Initial commit

---
+++ src/components/SocialMediaIcons.jsx
@@ -0,0 +1,50 @@
+const SocialMediaIcons = () => {
+    return (
+      <div className="flex justify-center md:justify-start my-10 gap-7">
+         <a
+          className="hover:opacity-50 transition duration-500"
+          href="https://github.com/Josianjidile"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img alt="github-link" src="../assets/github.png" />
+        </a>
+        <a
+          className="hover:opacity-50 transition duration-500"
+          href="https://www.linkedin.com/in/josia-njidile-05b7b6250"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img alt="linkedin-link" src="../assets/linkedin.png" />
+        </a>
+        <a
+          className="hover:opacity-50 transition duration-500"
+          href="https://www.instagram.com/_jcee._/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img alt="instagram-link" src="../assets/instagram.png" />
+        </a>
+        <a
+          className="hover:opacity-50 transition duration-500"
+          href="https://www.twitter.com"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img alt="twitter-link" src="../assets/twitter.png" />
+        </a>
+        <a
+          className="hover:opacity-50 transition duration-500"
+          href="https://www.facebook.com"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img alt="facebook-link" src="../assets/facebook.png" />
+        </a>
+        
+      </div>
+    );
+  };
+  
+  export default SocialMediaIcons;
+  
---
+++ src/scenes/Landing.jsx
@@ -0,0 +1,142 @@
+import React from "react";
+// import { FaFileDownload } from "react-icons/fa"; // Import the download icon
+import SocialMediaIcons from "../components/SocialMediaIcons";
+import useMediaQuery from "../hooks/useMediaQuery";
+import { motion } from "framer-motion";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+
+const Landing = ({ setSelectedPage }) => {
+  const isAboveLarge = useMediaQuery("(min-width: 1060px)");
+  
+  return (
+    <section
+      id="home"
+      className="md:flex md:justify-between md:items-center gap-16 md:h-full py-10"
+    >
+      {/* IMAGE SECTION */}
+      <div className="basis-3/5 z-10 mt-16 md:mt-32 flex justify-center md:order-2">
+        {isAboveLarge ? (
+          <div
+            className="relative z-0 ml-20 before:absolute before:-top-20 before:-left-20 before:rounded-t-[400px]
+            before:w-full before:max-w-[400px] md:before:max-w-[600px] before:h-full before:border-2 before:border-blue before:z-[-1]"
+          >
+            <img
+              alt="profile"
+              className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]"
+              src="assets/profile-image.png"
+            />
+          </div>
+        ) : (
+          <img
+            alt="profile"
+            className="z-10 w-full max-w-[400px] md:max-w-[600px]"
+            src="assets/profile-image.png"
+          />
+        )}
+      </div>
+
+      {/* MAIN TEXT */}
+      <div className="z-30 basis-2/5 mt-12 md:mt-32">
+        {/* HEADINGS */}
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <p className="text-6xl font-playfair z-10 text-center md:text-start">
+            Josia {""}
+            <span
+              className="xs:relative xs:text-deep-blue xs:font-semibold z-20 xs:before:content-brush
+              before:absolute before:-left-[25px] before:-top-[70px] before:z-[-2]"
+            >
+              Njidile
+            </span>
+          </p>
+
+          <p className="mt-10 mb-7 text-sm text-center md:text-start">
+            As a junior MERN (MongoDB, Express.js, React.js, Node.js) developer, I have a strong foundation in web
+            development. I am proficient in building responsive and interactive web applications using React.js for
+            front-end development, Express.js for server-side logic, Node.js for backend development, and MongoDB
+            for database management. I am eager to contribute to a collaborative team, deliver high-quality code,
+            and create engaging user experiences.
+          </p>
+        </motion.div>
+
+        {/* CALL TO ACTIONS */}
+        <motion.div
+          className="flex mt-5 justify-center md:justify-start"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <AnchorLink
+            className="bg-gradient-rainblue text-deep-blue rounded-sm py-3 px-7 font-semibold
+              hover:bg-blue hover:text-white transition duration-500"
+            onClick={() => setSelectedPage("contact")}
+            href="#contact"
+          >
+            Contact Me
+          </AnchorLink>
+          <AnchorLink
+            className="rounded-r-sm bg-gradient-rainblue py-0.5 pr-0.5"
+            onClick={() => setSelectedPage("contact")}
+            href="#contact"
+          >
+            <div className="bg-deep-blue hover:text-red transition duration-500 w-full h-full flex items-center justify-center px-10 font-playfair">
+              Let's talk.
+            </div>
+          </AnchorLink>
+        </motion.div>
+
+        {/* Download CV Button */}
+        <motion.div
+          className="flex mt-5 justify-center md:justify-start"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.6, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <a
+            href="cv/Josia Njidile's CV.pdf" // Replace with the actual path or URL to your CV file
+            target="_blank" // Open in a new tab
+            download="Josia Njidile's CV.pdf" // Set the desired file name for download
+            className="bg-gradient-rainblue text-deep-blue rounded-sm py-3 px-7 font-semibold
+              hover:bg-blue hover:text-white transition duration-500 flex items-center"
+          >
+           Download CV
+          </a>
+        </motion.div>
+
+        <motion.div
+          className="flex mt-5 justify-center md:justify-start"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.4, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <SocialMediaIcons />
+        </motion.div>
+      </div>
+    </section>
+  );
+};
+
+export default Landing;
